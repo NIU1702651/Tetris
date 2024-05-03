32425d11e36db4c1e1672cd26d1fbe53c39ef8db
NIU1702651 <149039588+NIU1702651@users.noreply.github.com>
2024-05-03 21:37:56 +0200

    Update Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -1,293 +1,150 @@
 #include "Tauler.h"
-void Tauler::crearTauler(const int& fila, const string& linia)
+
+// Constructor bàsic, posa el tauler tot de color negre.
+
+Tauler::Tauler()
 {
-	for (int f = 0; f < MAX_FILA; f++)
-	{
-		for (int c = 0; c < MAX_COL; c++)
-		{
-			m_tauler[f][c] = COLOR_NEGRE;
-		}
-	}
+	for (int i = 0; i < MAX_COL; i++)
+		for (int j = 0; j < MAX_FILA; j++)
+		
+			m_tauler[i][j] = COLOR_NEGRE;
+			
 }
-bool Tauler::movientValid_H(const Figura* figura, bool dreta)
+
+
+// Constructor que copia un tauler donat.
+
+Tauler::Tauler(ColorFigura tauler[MAX_COL][MAX_FILA])
 {
-	int Y = figura->getFila();
-	int X = figura->getColumna();
-	bool valid = false;
-	int limit = dreta ? X + figura->getMaxColumna() - figura->getColumnaCentre() : X -
-		figura->getMaxColumna() + figura->getColumnaCentre();
-	if (dreta ? limit <= MAX_COL : limit >= 0)
+	for (int i = 0; i < MAX_COL; i++)
+		for (int j = 0; j < MAX_FILA; j++)
+			m_tauler[i][j] = tauler[i][j];
+}
+
+
+// Verifica si una posició està dins dels límits del tauler i és buida.
+
+bool Tauler::posicioValida(const Posicio& pos) const
+{
+	bool esValida = true;
+
+	if (0 > pos.horitzontal || pos.horitzontal >= MAX_FILA || pos.vertical >= MAX_COL)
+	
+		esValida = false;
+		
+	else
 	{
-		TipusFigura tFig = figura->getTipus();
-		int gir = figura->getGir();
-		if (tFig == FIGURA_I)
-			if (gir == 0 || gir == 2)
-				valid = m_tauler[Y][dreta ? X + 3 : X - 2] == 0;
-			else
+	    
+		if (m_tauler[pos.vertical][pos.horitzontal] != COLOR_NEGRE)
+			esValida = false;
+			
+	}
+
+	return esValida;
+}
+
+
+// Verifica si una figura pot moure's a una posició determinada sense sortir del tauler ni ocupar caselles ocupades.
+
+bool Tauler::esMovimentValid(const Figura& figura, const Posicio& pos) const
+{
+	bool esValid = true;
+	int i = 0;
+
+	while (i < figura.getMida() && esValid)
+	{
+		int j = 0;
+		while (j < figura.getMida() && esValid)
+		{
+			if (figura.getFigura(i, j) != NO_COLOR)
 			{
-				int xT = dreta ? X + 1 : X - 1;
-				valid = m_tauler[Y - 1][xT] == 0 &&
-					m_tauler[Y][xT] == 0 && m_tauler[Y + 1][xT] == 0 &&
-					m_tauler[Y + 2][xT] == 0;
+			    
+				Posicio pos_tmp;
+				pos_tmp.vertical = pos.vertical + i;
+				pos_tmp.horitzontal = pos.horitzontal + j;
+
+				esValid = posicioValida(pos_tmp);
+				
 			}
-		else if (tFig == FIGURA_O)
-		{
-			int xT = dreta ? X + 1 : X - 1;
-			valid = m_tauler[Y][xT] == 0 && m_tauler[Y + 1][xT] == 0;
+			
+			j++;
 		}
-		else if (tFig == FIGURA_T || tFig == FIGURA_L || tFig == FIGURA_J)
+		
+		i++;
+	}
+
+	return esValid;
+}
+
+// Elimina files completes i desplaça les files superiors.
+int Tauler::eliminarFilesCompletades()
+{
+	int nCompletades = 0;
+
+	for (int i = 0; i < MAX_FILA; ++i)
+	{
+		bool completa = true;
+		int j = 0;
+
+		while (j < MAX_COL && completa)
 		{
-			if (gir == 0 || gir == 2)
+			if (m_tauler[i][j] == 0) {
+				completa = false;
+			}
+			j++;
+		}
+
+		if (completa)
+		{
+			nCompletades++;
+			for (int fila = i; fila > 0; --fila)
+				for (int k = 0; k < MAX_COL; ++k)
+				
+					m_tauler[fila][k] = m_tauler[fila - 1][k];
+					
+
+			for (int k = 0; k < MAX_COL; ++k)
+			
+				m_tauler[0][k] = COLOR_NEGRE;
+		}
+
+	}
+	
+	return nCompletades;
+}
+
+// Elimina una figura del tauler.
+
+void Tauler::eliminarFigura(Figura figura, const Posicio& pos)
+{
+	for (int i = 0; i < figura.getMida(); i++)
+		for (int j = 0; j < figura.getMida(); j++)
+		
+			if (figura.getFigura(i, j) != NO_COLOR)
 			{
-				int xT = dreta ? X + 2 : X - 2;
-				valid = m_tauler[Y][dreta ? X + 2 : X - 2] == 0 &&
-					m_tauler[dreta ? Y + 1 : Y - 1][dreta ? tFig == FIGURA_L ? X : tFig ==
-					FIGURA_T ? X + 1 : X + 2 : tFig == FIGURA_L ? X : tFig == FIGURA_T ? X - 1 : X - 2] == 0;
+			    
+			   
+				m_tauler[pos.vertical + i][pos.horitzontal + j] = COLOR_NEGRE;
+				
 			}
-			else
+}
+
+
+// Col·loca una figura en una posició sense verificar si és vàlida.
+
+void Tauler::collocarFigura(Figura figura, const Posicio& pos)
+{
+	for (int i = 0; i < figura.getMida(); i++)
+		for (int j = 0; j < figura.getMida(); j++)
+		{
+		    
+			if (figura.getFigura(i, j) != NO_COLOR)
 			{
-				int xT = dreta ? X + 1 : X - 1;
-				valid = m_tauler[Y - 1][gir == 1 ? tFig == FIGURA_L ? X - 2 : xT : tFig == FIGURA_J
-					? X - 2 : xT] == 0 &&
-					m_tauler[Y][tFig == FIGURA_T ? X - 2 : xT] == 0 &&
-					m_tauler[Y + 1][gir == 1 ? tFig == FIGURA_J ? X - 2 : xT : tFig == FIGURA_L ?
-					X - 2 : xT] == 0;
+			    
+				m_tauler[pos.vertical + i][pos.horitzontal + j] = figura.getColor();
+				
 			}
 		}
-		else if (tFig == FIGURA_S || tFig == FIGURA_Z)
-		{
-			if (gir == 0 || gir == 2)
-			{
-				int xT = dreta ? tFig == FIGURA_S ? X + 2 : X + 1 : tFig == FIGURA_S ? X - 1 : X - 2;
-				int xT2 = dreta ? tFig == FIGURA_S ? X + 1 : X + 2 : tFig == FIGURA_S ? X - 2 : X - 1;
-				valid = m_tauler[Y][xT] == 0 &&
-					m_tauler[Y + 1][xT2] == 0;
-			}
-			else
-			{
-				int xT = dreta ? X + 1 : X - 2;
-				valid = m_tauler[Y - 1][dreta ? tFig == FIGURA_S ? X : xT : tFig == FIGURA_S ? xT
-					: X - 1] == 0 &&
-					m_tauler[Y][xT] == 0 &&
-					m_tauler[Y + 1][dreta ? tFig == FIGURA_S ? xT : X : tFig == FIGURA_S ? X - 1
-					: xT] == 0;
-			}
-		}
-	}
-	return valid;
-}
-bool Tauler::movientValid_V(const Figura* figura)
-{
-	int X = figura->getColumna();
-	int Y = figura->getFila();
-	bool valid = false;
-	TipusFigura tFig = figura->getTipus();
-	int gir = figura->getGir();
-	if (tFig == FIGURA_O)
-		valid = m_tauler[Y + 2][X - 1] == 0 && m_tauler[Y + 2][X] == 0;
-	else if (tFig == FIGURA_I)
-	{
-		if (gir == 0 || gir == 2)
-		{
-			valid = m_tauler[Y + 1][X - 1] == 0 &&
-				m_tauler[Y + 1][X] == 0 &&
-				m_tauler[Y + 1][X + 1] == 0 &&
-				m_tauler[Y + 1][X + 2] == 0;
-		}
-		else
-			valid = m_tauler[Y + 3][X] == 0;
-	}
-	else if (tFig == FIGURA_S || tFig == FIGURA_Z)
-	{
-		if (gir == 0 || gir == 2)
-		{
-			valid = m_tauler[tFig == FIGURA_Z ? Y + 1 : Y + 2][X - 1] == 0 &&
-				m_tauler[Y + 2][X] == 0 &&
-				m_tauler[tFig == FIGURA_S ? Y + 1 : Y + 2][X + 1] == 0;
-		}
-		else
-		{
-			valid = m_tauler[tFig == FIGURA_Z ? Y + 2 : Y + 1][X - 1] == 0 &&
-				m_tauler[tFig == FIGURA_S ? Y + 1 : Y + 2][X] == 0;
-		}
-	}
-	else {
-		if (gir == 0 || gir == 2)
-		{
-			valid = m_tauler[gir == 0 && tFig == FIGURA_L ? Y + 2 : Y + 1][X - 1] == 0 &&
-				m_tauler[gir == 0 && tFig == FIGURA_T ? Y + 2 : Y + 1][X] == 0 &&
-				m_tauler[gir == 0 && tFig == FIGURA_J ? Y + 2 : Y + 1][X + 1] == 0;
-		}
-		else
-		{
-			int yT = gir == 1 ? tFig == FIGURA_L ? Y : tFig == FIGURA_T ? Y + 1 : Y + 2 : tFig ==
-				FIGURA_L ? Y + 2 : tFig == FIGURA_T ? Y + 1 : Y;
-			valid = m_tauler[yT][X - 1] == 0 &&
-				m_tauler[Y + 2][X] == 0;
-		}
-	}
-	return valid;
-}
-bool Tauler::girValid(const Figura* figura, DireccioGir& direccio)
-{
-	int X = figura->getColumna();
-	int Y = figura->getFila();
-	bool valid = false;
-	TipusFigura tFig = figura->getTipus();
-	int gir = figura->getGir();
-	if (tFig == FIGURA_I)
-	{
-		if (gir % 2 == 0)
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y + 1][X] == 0 &&
-			m_tauler[Y + 2][X] == 0;
-		else
-			valid = m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0 &&
-			m_tauler[Y][X + 2] == 0;
-	}
-	else if (tFig == FIGURA_S || tFig == FIGURA_Z)
-	{
-		if (gir % 2 == 0)
-		{
-			if (tFig == FIGURA_S)
-				valid = m_tauler[Y][X - 1] == 0 &&
-				m_tauler[Y - 1][X - 1] == 0;
-			else
-				valid = m_tauler[Y + 1][X] == 0 && m_tauler[Y - 1][X - 1] == 0;
-		}
-		else
-		{
-			if (tFig == FIGURA_S)
-				valid = m_tauler[Y][X + 1] == 0 &&
-				m_tauler[Y - 1][X - 1] == 0;
-			else
-				valid = m_tauler[Y - 1][X] == 0 &&
-				m_tauler[Y - 1][X + 1] == 0;
-		}
-	}
-	else if (tFig == FIGURA_L)
-	{
-		if ((gir == 0 && direccio == GIR_HORARI) || (gir == 2 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 1
-			valid = m_tauler[Y - 1][X - 1] == 0 &&
-			m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y + 1][X] == 0;
-		else if ((gir == 1 && direccio == GIR_HORARI) || (gir == 3 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 2
-			valid = m_tauler[Y - 1][X + 1] == 0 &&
-			m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0;
-		else if ((gir == 2 && direccio == GIR_HORARI) || (gir == 0 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 3
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y + 1][X] == 0 &&
-			m_tauler[Y + 1][X + 1] == 0;
-		else if ((gir == 3 && direccio == GIR_HORARI) || (gir == 1 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 0
-			valid = m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0 &&
-			m_tauler[Y + 1][X - 1] == 0;
-	}
-	else if (tFig == FIGURA_T)
-	{
-		if ((gir == 0 && direccio == GIR_HORARI) || (gir == 2 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 1
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y + 1][X] == 0;
-		else if ((gir == 1 && direccio == GIR_HORARI) || (gir == 3 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 2
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0; else if ((gir == 2 && direccio == GIR_HORARI) || (gir == 0 && direccio ==
-				GIR_ANTI_HORARI)) //Mierm Estat 3
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y][X + 1] == 0 &&
-			m_tauler[Y + 1][X] == 0;
-			else if ((gir == 3 && direccio == GIR_HORARI) || (gir == 1 && direccio ==
-				GIR_ANTI_HORARI)) //Mierm Estat 0
-			valid = m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0 &&
-			m_tauler[Y + 1][X] == 0;
-	}
-	else // if (tFig == FIGURA_J)
-	{
-		if ((gir == 0 && direccio == GIR_HORARI) || (gir == 2 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 1
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y + 1][X - 1] == 0 &&
-			m_tauler[Y + 1][X] == 0;
-		else if ((gir == 1 && direccio == GIR_HORARI) || (gir == 3 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 2
-			valid = m_tauler[Y - 1][X - 1] == 0 &&
-			m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0;
-		else if ((gir == 2 && direccio == GIR_HORARI) || (gir == 0 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 3
-			valid = m_tauler[Y - 1][X] == 0 &&
-			m_tauler[Y - 1][X + 1] == 0 &&
-			m_tauler[Y + 1][X] == 0;
-		else if ((gir == 3 && direccio == GIR_HORARI) || (gir == 1 && direccio ==
-			GIR_ANTI_HORARI)) //Mierm Estat 0
-			valid = m_tauler[Y][X - 1] == 0 &&
-			m_tauler[Y][X + 1] == 0 &&
-			m_tauler[Y + 1][X + 1] == 0;
-	}
-	return valid;
-}
-int Tauler::setTauler(Figura* figura)
-{
-	int f_fila = -1, f_columna;
-	for (int f = figura->getFila() - figura->getFilaCentre(); f < (figura->getFila() -
-		figura->getFilaCentre() + figura->getMaxFila()); f++)
-	{
-		f_fila++; f_columna = -1;
-		for (int c = figura->getColumna() - figura->getColumnaCentre(); c <
-			(figura->getColumna() - figura->getColumnaCentre() + figura->getMaxColumna()); c++)
-		{
-			f_columna++;
-			m_tauler[f][c] = ColorFigura(m_tauler[f][c] + figura->getCelaFigura(f_fila, f_columna));
-		}
-	}
-	figura->setAlTauler(true);
-	return borrarFilesCompletes();
-}
-void Tauler::mostraTauler()
-{
-	for (int f = 0; f < MAX_FILA; f++)
-	{
-		for (int c = 0; c < MAX_COL; c++)
-		{
-			if (c != 0)
-				cout << " ";
-			cout << m_tauler[f][c];
-		}
-		cout << endl;
-	}
-}
-vector<int> Tauler::filesCompletes()
-{
-	vector<int> d;
-	for (int f = 0; f < MAX_FILA; f++)
-	{
-		bool borrar = false;
-		for (int c = 0; c < MAX_COL && !borrar; c++)
-		{
-			borrar = !(m_tauler[f][c] == 0);
-		}
-	}
-	return d;
-}
-int Tauler::borrarFilesCompletes()
-{
-	vector<int> d = filesCompletes();
-	for (int i = d.size() - 1; i >= 0; i++)
-	{
-		for (int c = 0; c < MAX_COL; c++)
-		{
-			m_tauler[d[i]][c] = m_tauler[d[i] - 1][c];
-		}
-	}
-	return d.size();
+		
+	figura.setPosicio(pos);
 }
